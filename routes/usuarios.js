5ad6bc72d3796c5a8988fefbcdc9129f892de681
Emilio <emilio17mc@gmail.com>
2022-05-07 16:01:47 -0500

    Primer commit. Backend básico

---
+++ routes/usuarios.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const {
+  usuariosGet,
+  usuariosDelete,
+  usuariosPatch,
+  usuariosPut,
+  usuariosPost,
+} = require("../controllers/usuarios");
+
+const router = Router();
+
+router.get("/", usuariosGet);
+
+router.put("/:id", usuariosPut);
+
+router.post("/", usuariosPost);
+
+router.delete("/:id", usuariosDelete);
+
+router.patch("/", usuariosPatch);
+
+module.exports = router;
